2c6813e170388c4bc7e7558e10c57b2115f86be0
ChethiyaKaushal <chethiyakaushal1@gmail.com>
2026-05-31 13:53:43 +0530

    create tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,13 +1,32 @@
+create database PSMS;
+use psms;
+
 CREATE TABLE Province (
-    Province_ID INT PRIMARY KEY,
+    Province_ID varchar(10) PRIMARY KEY,
     Province_Name VARCHAR(50),
     Province_Code VARCHAR(10)
 );
 
 CREATE TABLE District (
-    District_ID INT PRIMARY KEY,
+    District_ID varchar(10) PRIMARY KEY,
     District_Name VARCHAR(50),
-    Province_ID INT,
+    Province_ID varchar(10),
     FOREIGN KEY (Province_ID)
     REFERENCES Province(Province_ID)
+);
+
+CREATE TABLE ATHLETE (
+    National_ID VARCHAR(20) NOT NULL,
+    Athlete_ID VARCHAR(10) PRIMARY KEY,
+    First_Name VARCHAR(50) NOT NULL,
+    Last_Name VARCHAR(50) NOT NULL,
+
+    Athlete_Name VARCHAR(101)
+        GENERATED ALWAYS AS (CONCAT(First_Name, ' ', Last_Name)) STORED,
+
+    REG_No VARCHAR(10) NOT NULL,
+    Date_of_Birth DATE,
+    Gender VARCHAR(10),
+    Age INT,
+    Contact_No VARCHAR(15) NOT NULL
 );
